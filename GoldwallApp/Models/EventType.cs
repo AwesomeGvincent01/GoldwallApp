87a4bcaf7531a5d5a612cf23e2e4991a13991ef7
AwesomeGvincent01 <ac148838@avcol.school.nz>
2026-03-21 17:17:46 +1300

    working on EventType and WorkEvent entities

---
+++ GoldwallApp/Models/EventType.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+  namespace GoldwallApp.Models;
+
+    public class EventType {
+
+
+    //pk
+    public int EventTypeId { get; set; }
+
+
+    //business fk
+    public int BusinessId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Categoery { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public Business Business { get; set; }
+
+    public ICollection<WorkEvent> WorkEvents { get; set; }
+
+}
+}
---
+++ GoldwallApp/Models/WorkEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoldwallApp.Models
+{
+    public class WorkEvent
+    {
+
+
+        //pk
+        public int WorkEventId { get; set; }
+
+        [Display(Name = "Event Type")]
+        public int SurfaceId { get; set; }
+
+        public int UserId { get; set; } 
+
+        public int EventTypeId { get; set; }
+
+        public DateTime StartedAt { get; set; }
+
+        public DateTime EndedAt { get; set; }
+
+        public string Notes { get; set; }
+
+        public Surface Surface { get; set; }
+
+        public User User { get; set; }
+
+        public EventType EventType { get; set; }
+    }
+}
