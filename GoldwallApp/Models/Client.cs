7bfd35eb52c836ac3a4d264cbb8fca47037dc2ea
AwesomeGvincent01 <ac148838@avcol.school.nz>
2026-04-05 15:37:25 +1200

    Comments for a few annotations in client/job

--- GoldwallApp/Models/Client.cs
+++ GoldwallApp/Models/Client.cs
@@ -9,14 +9,14 @@ namespace GoldwallApp.Models
         [Display(Name = "Business")]
         public int BusinessId { get; set; }
 
-        [Required]
+        [Required] //required so every client record identifies client correctly
         [StringLength(100)]
         public string? Name { get; set; }
 
-        [Required]
+        [Required] //required because contact details are very important for jobs and communication with clients
         [Phone]
        [StringLength(30)]
-        public string? Phone { get; set; }
+        public string? Phone { get; set; } 
 
         [EmailAddress]
         [StringLength(100)]
--- GoldwallApp/Models/Job.cs
+++ GoldwallApp/Models/Job.cs
@@ -12,14 +12,18 @@ namespace GoldwallApp.Models
         [Display(Name = "Client")]
         public int ClientId { get; set; }
 
-        [Required]
+
+
+        
+        [Required] //required because every job needs a title to identify it and differentiate it from other jobs
         [StringLength(100)]
         public string? Title { get; set; }
 
-        [Required]
+        [Required] //required because every job needs an address to identify where the work is taking place
         [StringLength(150)]
         public string? Address { get; set; }
 
+        //required because every job needs a status to identify where it is in the workflow and to help with organization and tracking
         [Required]
         [StringLength(30)]
         public string? Status { get; set; }
