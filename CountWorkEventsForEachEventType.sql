6e9642784830eccc05986ffd23bfe13cdda7543c
AwesomeGvincent01 <ac148838@avcol.school.nz>
2026-04-08 19:26:29 +1200

    Advanced Query 3 & 4
    A3 - Count work events for each event type.
    
    A4 - Total surface area for each room. Uses the SUM aggregate function to add up total surface area inside each  room.

---
+++ CountWorkEventsForEachEventType.sql
@@ -0,0 +1,6 @@
+﻿SELECT EventType.Name, COUNT(WorkEvent.WorkEventId) AS WorkEventCount
+FROM EventType
+INNER JOIN WorkEvent
+    ON EventType.EventTypeId = WorkEvent.EventTypeId
+GROUP BY EventType.Name
+ORDER BY WorkEventCount DESC;
---
+++ SurfaceAreaForEachRoom.sql
@@ -0,0 +1,6 @@
+﻿SELECT Room.Name, SUM(Surface.AreaM2) AS TotalSurfaceArea
+FROM Room
+INNER JOIN Surface
+    ON Room.RoomId = Surface.RoomId
+GROUP BY Room.Name
+ORDER BY TotalSurfaceArea DESC;
