d4e27e127c748b2603cae5665da89cc3d89a1fff
AwesomeGvincent01 <ac148838@avcol.school.nz>
2026-04-09 12:32:32 +1200

    Advanced Query 5
    Final advanced query. Counts defect reports by defect type name. Also uses "as" command to make counted defect reports appear as defect count

---
+++ CountDefectReportByDefectType.sql
@@ -0,0 +1,6 @@
+﻿SELECT DefectType.Name, COUNT(DefectReport.DefectReportId) AS DefectCount
+FROM DefectType
+INNER JOIN DefectReport
+    ON DefectType.DefectTypeId = DefectReport.DefectTypeId
+GROUP BY DefectType.Name
+ORDER BY DefectCount DESC;
