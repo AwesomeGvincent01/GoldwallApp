37504a410d6e173964773094d414f604428bc835
AwesomeGvincent01 <ac148838@avcol.school.nz>
2026-04-10 20:56:26 +1200

    Complex Query 1
    Shows event types that have more than 1 work event.
    
    Counts how many work event each event type has, then groups the results using "group by" by event type name, then uses "having COUNT (WorkEvent.WorkEventId) > 1" to disclude event types that have less than 2 workevents.

---
+++ ShowEventTypeWithOver1WorkEvent.sql
@@ -0,0 +1,7 @@
+﻿SELECT EventType.Name, COUNT(WorkEvent.WorkEventId) AS WorkEventCount
+FROM EventType
+INNER JOIN WorkEvent
+    ON EventType.EventTypeId = WorkEvent.EventTypeId
+GROUP BY EventType.Name
+HAVING COUNT(WorkEvent.WorkEventId) > 1
+ORDER BY WorkEventCount DESC;
