08a8711ad715c03b2b78227fdfdd02db97a844f5
AwesomeGvincent01 <ac148838@avcol.school.nz>
2026-04-19 18:24:25 +1200

    Experimenting with LINQ

---
+++ GoldwallApp/Data/LINQ.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using GoldwallApp.Data;
+
+namespace GoldwallApp.Data
+{
+    public class LinqExamples
+    {
+        private readonly AppDbContext _context;
+
+        public LinqExamples(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public object ExampleQueries()
+        {
+            // LINQ query 1
+            //filters jobs to include only those with a status of "Active"
+            var activeJobs = _context.Jobs
+                .Where(j => j.Status == "Active")
+                .ToList();
+
+            // LINQ query 2:
+            // sorts jobs by their title in ascending order
+            var orderedJobs = _context.Jobs
+                .OrderBy(j => j.Title)
+                .ToList();
+
+            return new
+            {
+                ActiveJobs = activeJobs,
+                OrderedJobs = orderedJobs
+            };
+        }
+    }
+}
